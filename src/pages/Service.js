5666c7067d349da37a42a993f57e8481faf8b4b7
a945936939 <945936939@qq.com>
2023-08-12 01:15:52 +1000

    updated portfolio section

--- src/pages/Service.js
+++ src/pages/Service.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-import design from "public/design.png";
+
+import code from "public/code.png";
+
 import Image from "next/image";
 export default function Service(props) {
   return (
     <div className="lg:w-1/3 flex-auto  text-center shadow-lg p-10 rounded-xl my-10">
-      <Image src={design} width={100} height={100} className="mx-auto" />
+      <Image src={props.src} width={100} height={100} className="mx-auto" />
 
-      <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
+      <h3 className="text-lg font-medium pt-8 pb-2">{props.title}</h3>
       <p className="text-md py-2 leading-8 text-gray-800">{props.content}</p>
       <h4></h4>
     </div>
